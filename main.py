ab024fe86b63c5bb898b302385b42fcd8150052e
Damian Rakus <damian.rakus@gmail.com>
2021-04-17 19:02:21 +0200

    something is being painted

--- main.py
+++ main.py
@@ -8,11 +8,39 @@ import numpy as np
 from PIL import Image, ImageDraw
 from geojson_to_pixel_transformer import GeoJsonToPixelTransformer
 import argparse
+import math
 
+CAMERA_LENS = 35
+SENSOR_WIDTH = 35.9
+SENSOR_HEIGHT = 24
+
+EARTH_RADIUS = 6_371_000
 
 
 def get_image_metadata_dataframe(path: str):
     return pd.read_csv(path, sep="\t")
+
+def get_rotation_matrix(yaw: float, pitch: float, roll:float) -> np.ndarray:
+
+    #yaw, roll = roll, yaw # because we use relative to camera angles not plane
+
+    yaw = math.radians(yaw)
+    pitch = math.radians(pitch)
+    roll = math.radians(roll)
+
+    cos_a = math.cos(yaw)
+    cos_b = math.cos(pitch)
+    cos_c = math.cos(roll)
+
+    sin_a = math.sin(yaw)
+    sin_b = math.sin(pitch)
+    sin_c = math.sin(roll)
+
+    return np.array([
+        [cos_a * cos_b, cos_a * sin_b * sin_c - sin_a * cos_c, cos_a * sin_b * cos_c + sin_a * sin_c],
+        [sin_a * cos_b, sin_a * sin_b * sin_c + cos_a * cos_c, sin_a * sin_b * cos_c - cos_a * sin_c],
+        [-sin_b, cos_b * sin_c, cos_b * cos_c]
+    ])
 
 
 def get_image_corners(img_metadata):
@@ -30,14 +58,54 @@ def get_image_corners(img_metadata):
     # 0 = straight, plane pointing to the sky
     pitch = img_metadata['pitch[deg]']
 
-    to_modify = 0.005
-    # top left corner first, then clockwise
-    return Polygon([(img_metadata.longitude - to_modify, img_metadata.latitude + to_modify),
-            (img_metadata.longitude + to_modify, img_metadata.latitude + to_modify),
-            (img_metadata.longitude + to_modify, img_metadata.latitude - to_modify),
-            (img_metadata.longitude - to_modify, img_metadata.latitude - to_modify),
-            (img_metadata.longitude - to_modify, img_metadata.latitude + to_modify)])
+    # https://stackoverflow.com/questions/17599845/camera-projection-on-a-plane
+    camera_vertical_angle = 2 * math.atan(SENSOR_HEIGHT/(2 * CAMERA_LENS)) # in radians
+    camera_horizontal_angle = 2 * math.atan(SENSOR_WIDTH/(2 * CAMERA_LENS)) # in radians
 
+    camera_rotation_matrix = get_rotation_matrix(yaw, pitch, roll)
+    
+    right = camera_rotation_matrix[:, 0]
+    up = camera_rotation_matrix[:, 1] 
+    away = camera_rotation_matrix[:, 2]
+
+    right_factor = math.tan(camera_horizontal_angle / 2)
+    up_factor = math.tan(camera_vertical_angle / 2)
+
+    top_left_vector = away - right_factor * right + up_factor * up
+    top_right_vector = away + right_factor * right + up_factor * up
+    bottom_left_vector = away - right_factor * right - up_factor * up
+    bottom_right_vector = away + right_factor * right - up_factor * up
+
+    time_top_left =  -drone_height / top_left_vector[2]
+    time_top_right = -drone_height / top_right_vector[2]
+    time_bottom_left = -drone_height / bottom_left_vector[2]
+    time_bottom_right = -drone_height / bottom_right_vector[2]
+
+    if time_top_left < 0 or time_top_right < 0 or time_bottom_left < 0 or time_bottom_right:
+        pass
+
+    top_left_diff = top_left_vector[0:2] * time_top_left
+    top_right_diff = top_right_vector[0:2] * time_top_right
+    bottom_left_diff = bottom_left_vector[0:2] * time_bottom_left
+    bottom_right_diff = bottom_right_vector[0:2] * time_bottom_right
+
+    latitude_factor = 1/one_meter_to_latitude()
+    longitude_factor = 1/one_meter_to_longitude(img_metadata.latitude)
+    
+    # top left corner first, then clockwise
+    return Polygon([(img_metadata.longitude + longitude_factor * top_left_diff[0], img_metadata.latitude + latitude_factor * top_left_diff[1]),
+            (img_metadata.longitude + longitude_factor * top_right_diff[0], img_metadata.latitude + latitude_factor * top_right_diff[1]),
+            (img_metadata.longitude + longitude_factor * bottom_right_diff[0], img_metadata.latitude + latitude_factor * bottom_right_diff[1]),
+            (img_metadata.longitude + longitude_factor * bottom_left_diff[0], img_metadata.latitude + latitude_factor * bottom_left_diff[1]),
+            (img_metadata.longitude + longitude_factor * top_left_diff[0], img_metadata.latitude + latitude_factor * top_left_diff[1])])
+
+def one_meter_to_latitude() -> float:
+    return (math.pi * EARTH_RADIUS) / 180
+    #return  360 / (2*math.pi * EARTH_RADIUS)
+
+def one_meter_to_longitude(latitude: float) -> float:
+    return (math.pi * EARTH_RADIUS * math.cos(math.radians(latitude))) / 180
+    #return 360 / (EARTH_RADIUS * 2 * math.pi * math.cos(math.radians(latitude)))
 
 def get_intersections_with_polygons(polygon_for_intersection: Polygon, polygons_array: "list[Polygon]"):
     intersections = []
@@ -82,6 +150,10 @@ if __name__ == "__main__":
     data_classes_as_polygons = [GeoJsonReader().load(join(geo_json_files_folder_path, path)) for path in geo_json_files]
 
     for img_filename in listdir(images_folder_path): 
+        #test
+        if img_filename != "DSC01170.JPG" and img_filename != "DSC01169.JPG":
+            continue
+        #test
         img_full_path = join(images_folder_path, img_filename)
         if not isfile(img_full_path) or not img_filename.endswith(".JPG"):
             continue
@@ -124,3 +196,8 @@ if __name__ == "__main__":
             if debug_mode:
                 train_image = Image.alpha_composite(img, train_image)
             train_image.save(f"{results_path}/out_{img_filename[:-4]}.png")
+
+        #test
+        if img_filename == "DSC01169.JPG":
+            break
+        #test
