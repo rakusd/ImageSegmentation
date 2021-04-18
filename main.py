2137bd269e5530a384fbc34b9e935b118d9b12d8
Damian Rakus <damian.rakus@gmail.com>
2021-04-18 20:11:53 +0200

    coords of file

--- main.py
+++ main.py
@@ -37,9 +37,10 @@ def get_rotation_matrix(yaw: float, pitch: float, roll:float) -> np.ndarray:
     sin_c = math.sin(roll)
 
     return np.array([
-        [cos_a * cos_b, cos_a * sin_b * sin_c - sin_a * cos_c, cos_a * sin_b * cos_c + sin_a * sin_c],
-        [sin_a * cos_b, sin_a * sin_b * sin_c + cos_a * cos_c, sin_a * sin_b * cos_c - cos_a * sin_c],
-        [-sin_b, cos_b * sin_c, cos_b * cos_c]
+        [cos_a * cos_b, cos_a * sin_b * sin_c - sin_a * cos_c, cos_a * sin_b * cos_c + sin_a * sin_c, 0],
+        [sin_a * cos_b, sin_a * sin_b * sin_c + cos_a * cos_c, sin_a * sin_b * cos_c - cos_a * sin_c, 0],
+        [-sin_b, cos_b * sin_c, cos_b * cos_c, 0],
+        [0, 0, 0, 1]
     ])
 
 
@@ -63,23 +64,40 @@ def get_image_corners(img_metadata):
     camera_horizontal_angle = 2 * math.atan(SENSOR_WIDTH/(2 * CAMERA_LENS)) # in radians
 
     camera_rotation_matrix = get_rotation_matrix(yaw, pitch, roll)
+
+    initial_camera_matrix = np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, -drone_height, 1]
+    ])
+
+    camera_rotate_below_drone_matrix = get_rotation_matrix(-60, 180, 0)
     
-    right = camera_rotation_matrix[:, 0]
-    up = camera_rotation_matrix[:, 1] 
-    away = camera_rotation_matrix[:, 2]
+    final_camera_matrix = camera_rotate_below_drone_matrix.dot(camera_rotation_matrix.dot(initial_camera_matrix))
+
+    # TO DO MIGHT BE CHANGED TO READ ROWS INSTEAD OF COLUMNS
+    # right = final_camera_matrix[:3, 0]
+    # up = final_camera_matrix[:3, 1] 
+    # away = final_camera_matrix[:3, 2]
+    right = final_camera_matrix[0, :3]
+    up = final_camera_matrix[1, :3] 
+    away = final_camera_matrix[2, :3]
+
 
     right_factor = math.tan(camera_horizontal_angle / 2)
     up_factor = math.tan(camera_vertical_angle / 2)
 
-    top_left_vector = away - right_factor * right + up_factor * up
-    top_right_vector = away + right_factor * right + up_factor * up
-    bottom_left_vector = away - right_factor * right - up_factor * up
-    bottom_right_vector = away + right_factor * right - up_factor * up
+    top_left_vector = away - right_factor * right + up_factor * up # SE
+    top_right_vector = away + right_factor * right + up_factor * up # NE
+    bottom_left_vector = away - right_factor * right - up_factor * up # SW
+    bottom_right_vector = away + right_factor * right - up_factor * up #NW
 
-    time_top_left =  -drone_height / top_left_vector[2]
-    time_top_right = -drone_height / top_right_vector[2]
-    time_bottom_left = -drone_height / bottom_left_vector[2]
-    time_bottom_right = -drone_height / bottom_right_vector[2]
+    # drone height is negative as z-axis is pointing to the ground
+    time_top_left =  -(-drone_height) / top_left_vector[2]
+    time_top_right = -(-drone_height) / top_right_vector[2]
+    time_bottom_left = -(-drone_height) / bottom_left_vector[2]
+    time_bottom_right = -(-drone_height) / bottom_right_vector[2]
 
     if time_top_left < 0 or time_top_right < 0 or time_bottom_left < 0 or time_bottom_right:
         pass
@@ -93,11 +111,23 @@ def get_image_corners(img_metadata):
     longitude_factor = 1/one_meter_to_longitude(img_metadata.latitude)
     
     # top left corner first, then clockwise
-    return Polygon([(img_metadata.longitude + longitude_factor * top_left_diff[0], img_metadata.latitude + latitude_factor * top_left_diff[1]),
-            (img_metadata.longitude + longitude_factor * top_right_diff[0], img_metadata.latitude + latitude_factor * top_right_diff[1]),
-            (img_metadata.longitude + longitude_factor * bottom_right_diff[0], img_metadata.latitude + latitude_factor * bottom_right_diff[1]),
-            (img_metadata.longitude + longitude_factor * bottom_left_diff[0], img_metadata.latitude + latitude_factor * bottom_left_diff[1]),
-            (img_metadata.longitude + longitude_factor * top_left_diff[0], img_metadata.latitude + latitude_factor * top_left_diff[1])])
+    # x is north in RPY coordinates therefore latitutde is added x coords
+    return Polygon([(img_metadata.longitude + longitude_factor * top_left_diff[1], img_metadata.latitude + latitude_factor * top_left_diff[0]),
+            (img_metadata.longitude + longitude_factor * top_right_diff[1], img_metadata.latitude + latitude_factor * top_right_diff[0]),
+            (img_metadata.longitude + longitude_factor * bottom_right_diff[1], img_metadata.latitude + latitude_factor * bottom_right_diff[0]),
+            (img_metadata.longitude + longitude_factor * bottom_left_diff[1], img_metadata.latitude + latitude_factor * bottom_left_diff[0]),
+            (img_metadata.longitude + longitude_factor * top_left_diff[1], img_metadata.latitude + latitude_factor * top_left_diff[0])])
+
+def get_polygon_reversed(polygon: Polygon, try_index: int) -> Polygon:
+    x_coords = polygon.exterior.coords.xy[0][0:4]
+    y_coords = polygon.exterior.xy[1][0:4]
+
+    return Polygon([
+        (x_coords[(0+try_index)%4], y_coords[(0+try_index)%4]),
+        (x_coords[(1+try_index)%4], y_coords[(1+try_index)%4]),
+        (x_coords[(2+try_index)%4], y_coords[(2+try_index)%4]),
+        (x_coords[(3+try_index)%4], y_coords[(3+try_index)%4])
+        ])
 
 def one_meter_to_latitude() -> float:
     return (math.pi * EARTH_RADIUS) / 180
@@ -151,7 +181,7 @@ if __name__ == "__main__":
 
     for img_filename in listdir(images_folder_path): 
         #test
-        if img_filename != "DSC01170.JPG" and img_filename != "DSC01169.JPG":
+        if img_filename != "DSC01170.JPG":
             continue
         #test
         img_full_path = join(images_folder_path, img_filename)
@@ -165,39 +195,42 @@ if __name__ == "__main__":
         metadata_of_img_to_process = image_metadata[image_metadata.Filename == img_filename].iloc[0]
         img_polygon = get_image_corners(metadata_of_img_to_process)
 
-        intersections_with_data_classes = [get_intersections_with_polygons(img_polygon, data_class_polygon) for data_class_polygon in data_classes_as_polygons]
+        copy_img_polygon = img_polygon
+        for try_index in range(1):
+            img_polygon = get_polygon_reversed(copy_img_polygon, try_index)
+            intersections_with_data_classes = [get_intersections_with_polygons(img_polygon, data_class_polygon) for data_class_polygon in data_classes_as_polygons]
 
-        object_transformer = PolygonMatrixTransformer()
+            object_transformer = PolygonMatrixTransformer()
         
-        geojson_points = object_transformer.transform_to_matrix(img_polygon)
-        img_points = np.array([
-            [0, img.size[0] - 1, img.size[0] - 1, 0],
-            [0, 0, img.size[1] - 1, img.size[1] - 1],
-            [1, 1, 1, 1]
-        ])
+            geojson_points = object_transformer.transform_to_matrix(img_polygon)
+            img_points = np.array([
+                [0, img.size[0] - 1, img.size[0] - 1, 0],
+                [0, 0, img.size[1] - 1, img.size[1] - 1],
+                [1, 1, 1, 1]
+            ])
 
-        coords_transformer = GeoJsonToPixelTransformer(geojson_points, img_points)
+            coords_transformer = GeoJsonToPixelTransformer(geojson_points, img_points)
 
-        train_image = Image.new('RGBA', (img.size[0], img.size[1]))
-        brush = ImageDraw.Draw(train_image)
+            train_image = Image.new('RGBA', (img.size[0], img.size[1]))
+            brush = ImageDraw.Draw(train_image)
 
-        is_any_intersection = False
-        for index, intersections_with_data_class  in enumerate(intersections_with_data_classes):
-            color = get_class_color(index, debug_mode)
+            is_any_intersection = False
+            for index, intersections_with_data_class  in enumerate(intersections_with_data_classes):
+                color = get_class_color(index, debug_mode)
 
-            for intersection in intersections_with_data_class:
-                is_any_intersection = True
-                intersection_matrix_pixels = coords_transformer.transform(object_transformer.transform_to_matrix(intersection))
-                intersection_pixels = zip(intersection_matrix_pixels[0], intersection_matrix_pixels[1])
+                for intersection in intersections_with_data_class:
+                    is_any_intersection = True
+                    intersection_matrix_pixels = coords_transformer.transform(object_transformer.transform_to_matrix(intersection))
+                    intersection_pixels = zip(intersection_matrix_pixels[0], intersection_matrix_pixels[1])
                 
-                brush.polygon(list(intersection_pixels), fill=color)
+                    brush.polygon(list(intersection_pixels), fill=color)
 
-        if is_any_intersection:
-            if debug_mode:
-                train_image = Image.alpha_composite(img, train_image)
-            train_image.save(f"{results_path}/out_{img_filename[:-4]}.png")
+            if is_any_intersection:
+                if debug_mode:
+                    train_image = Image.alpha_composite(img, train_image)
+                train_image.save(f"{results_path}/out_{img_filename[:-4]}_{try_index}.png")
 
         #test
-        if img_filename == "DSC01169.JPG":
+        if img_filename == "DSC01170.JPG":
             break
         #test
