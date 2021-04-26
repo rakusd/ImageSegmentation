258b158ca4ff843cec178bae17e363aea2e8e0a5
seba174 <seba174@posteo.net>
2021-04-26 21:45:52 +0200

    Added some prints and other small changes

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ from shapely.geometry import Polygon
 import numpy as np
 from PIL import Image, ImageDraw
 import argparse
+import datetime
 
+
+def print_message_with_time(message: str):
+    print(f'[{datetime.datetime.now().strftime("%H:%M:%S")}] {message}')
 
 
 def get_image_metadata_dataframe(path: str):
@@ -26,8 +30,8 @@ def get_intersections_with_polygons(polygon_for_intersection: Polygon, polygons_
                 intersections.append(intersection)
         except Exception as e:
             count += 1
-            print(e)
-    print(f"\n\nNumber of exceptions during calculating intersections {count}\n\n")
+            print(f'Exception: {e}')
+    print(f"Number of exceptions during calculating intersections {count}")
     return intersections
 
 
@@ -75,7 +79,6 @@ def transform_polygons_for_classes(classes_as_polygons: "list[list[Polygon]]", p
     return new_polygons_classes
 
 
-
 if __name__ == "__main__":
     # python main.py --path='F:\Download\WD_dane' --result='F:\Download\WD_dane\Out_Results' --debug=True
     parser = argparse.ArgumentParser()
@@ -94,64 +97,88 @@ if __name__ == "__main__":
     pmatrix_path = f'{data_path}/Processing/07_05_2020_WL/1_initial/params/07_05_2020_WL_pmatrix.txt'
     offset_path = f'{data_path}/Processing/07_05_2020_WL/1_initial/params/07_05_2020_WL_offset.xyz'
 
+
+    print_message_with_time('------ Starting program ------')
+
     geo_json_files_folder_path = f'{data_path}/Classes'
     geo_json_files = [f for f in listdir(geo_json_files_folder_path) if isfile(join(geo_json_files_folder_path, f)) and f.endswith(".geojson")] 
 
-    offset = load_offset(offset_path)
+    print_message_with_time('------ Starting metadata parsing ------')
 
-    image_metadata = get_image_metadata_dataframe(metadata_file_path)
+    offset = load_offset(offset_path)
+    images_metadata = get_image_metadata_dataframe(metadata_file_path)
     data_classes_as_polygons = [GeoJsonReader().load(join(geo_json_files_folder_path, path), offset) for path in geo_json_files]
     dictionary_with_p_matrices = PmatrixParser().parse(pmatrix_path)
 
-    for img_filename in listdir(images_folder_path): 
+    print_message_with_time('------ Finished metadata parsing ------')
+    print_message_with_time('------ Starting image processing ------')
 
-        img_full_path = join(images_folder_path, img_filename)
-        if not isfile(img_full_path) or not img_filename.endswith(".JPG"):
-            continue
+    exceptions_count = 0
+    image_index = 0
+    images_names = listdir(images_folder_path)
+    for img_filename in images_names:
+        image_index += 1
 
-        metadata_of_img_to_process = image_metadata[image_metadata.Filename == img_filename].iloc[0]
-        roll = abs(metadata_of_img_to_process['roll[deg]'])
-        pitch = abs(metadata_of_img_to_process['pitch[deg]'])
-        
-        if roll > 10 or pitch > 10:
-            continue
+        try:
+            print_message_with_time(f'\n--- Processing image {img_filename} [{image_index}/{len(images_names)}] ---')
 
-        img = Image.open(img_full_path)
-        if debug_mode:
-            img = img.convert("RGBA")
+            img_full_path = join(images_folder_path, img_filename)
+            if not isfile(img_full_path) or not img_filename.endswith(".JPG"):
+                print('Bad image file')
+                continue
 
-        p_matrix = dictionary_with_p_matrices.get(img_filename)
-        if p_matrix is None:
-            continue
+            metadata_of_img_to_process = images_metadata[images_metadata.Filename == img_filename].iloc[0]
+            roll = abs(metadata_of_img_to_process['roll[deg]'])
+            pitch = abs(metadata_of_img_to_process['pitch[deg]'])
+            
+            if roll > 10 or pitch > 10:
+                print('Roll > 10 or pitch > 10')
+                continue
 
-        img_polygon = Polygon([
-            [0, 0],
-            [img.size[0], 0],
-            [img.size[0], img.size[1]],
-            [0, img.size[1]],
-            [0, 0]
-        ])
-
-        data_classes_as_polygons_copy = transform_polygons_for_classes(data_classes_as_polygons, p_matrix, offset)
-        intersections_with_data_classes = [get_intersections_with_polygons(img_polygon, data_class_polygon) for data_class_polygon in data_classes_as_polygons_copy]
-
-        train_image = Image.new('RGBA', (img.size[0], img.size[1]))
-        brush = ImageDraw.Draw(train_image)
-
-        is_any_intersection = False
-        for index, intersections_with_data_class  in enumerate(intersections_with_data_classes):
-            color = get_class_color(index, debug_mode)
-
-            for intersection in intersections_with_data_class:
-                is_any_intersection = True
-                intersection_matrix_pixels = PolygonMatrixTransformer().transform_to_matrix(intersection)
-                intersection_pixels = list(zip(intersection_matrix_pixels[0], intersection_matrix_pixels[1]))
-
-                brush.polygon(intersection_pixels, fill=color)
-
-        if is_any_intersection:
+            img = Image.open(img_full_path)
             if debug_mode:
-                train_image = Image.alpha_composite(img, train_image)
-            train_image.save(f"{results_path}/out_{img_filename[:-4]}.png")
-            print(f"{results_path}/out_{img_filename[:-4]}.png")
+                img = img.convert("RGBA")
 
+            p_matrix = dictionary_with_p_matrices.get(img_filename)
+            if p_matrix is None:
+                print('Pmatrix is None')
+                continue
+
+            img_polygon = Polygon([
+                [0, 0],
+                [img.size[0], 0],
+                [img.size[0], img.size[1]],
+                [0, img.size[1]],
+                [0, 0]
+            ])
+
+            data_classes_as_polygons_copy = transform_polygons_for_classes(data_classes_as_polygons, p_matrix, offset)
+            intersections_with_data_classes = [get_intersections_with_polygons(img_polygon, data_class_polygon) for data_class_polygon in data_classes_as_polygons_copy]
+
+            train_image = Image.new('RGBA', (img.size[0], img.size[1]))
+            brush = ImageDraw.Draw(train_image)
+
+            is_any_intersection = False
+            for index, intersections_with_data_class  in enumerate(intersections_with_data_classes):
+                color = get_class_color(index, debug_mode)
+
+                for intersection in intersections_with_data_class:
+                    is_any_intersection = True
+                    intersection_matrix_pixels = PolygonMatrixTransformer().transform_to_matrix(intersection)
+                    intersection_pixels = list(zip(intersection_matrix_pixels[0], intersection_matrix_pixels[1]))
+
+                    brush.polygon(intersection_pixels, fill=color)
+
+            if is_any_intersection:
+                if debug_mode:
+                    train_image = Image.alpha_composite(img, train_image)
+                path = f"{results_path}/out{'_debug' if debug_mode else ''}_{img_filename[:-4]}.png"
+                train_image.save(path)
+            else:
+                print('No intersections in current image')
+        except Exception as e:
+            exceptions_count += 1
+            print(f'!!!!!! Processing image {img_filename} FAILED: {e}')
+
+    print(f'\n\nFinal exceptions count: {exceptions_count}')
+    print_message_with_time('------ Finished image processing ------')
