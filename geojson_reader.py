41c876c5aba6301e83d0c12bbfa34f8f9cadfc28
Wojciech Socha <socha.wojtek@gmail.com>
2021-06-03 19:39:56 +0200

    Update 03_02_20 annotations, fix geojson parsing

--- geojson_reader.py
+++ geojson_reader.py
@@ -10,7 +10,10 @@ class GeoJsonReader:
 
         polygons = []
         for feature in geo_json["features"]:
-            polygon_array = feature["geometry"]["coordinates"][0][0]
+            coordinates = feature["geometry"]["coordinates"]
+            if (len(coordinates) == 0 or len(coordinates[0]) == 0):
+                continue
+            polygon_array = coordinates[0][0]
             transformed_polygon_array = []
             for polygon_point in polygon_array:
                 transformed_coordinates = self.transform_wgs_to_epsg_2178(polygon_point[0], polygon_point[1])
